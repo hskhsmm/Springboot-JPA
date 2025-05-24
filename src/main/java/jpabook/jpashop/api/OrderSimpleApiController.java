66f6236571775b408e1079753c7f91fb5eed348a
hskhsmm <hskhsmm@naver.com>
2025-05-24 16:05:09 +0900

    feat: Fetch join 사용

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * V1. 엔티티 직접 노출
+ * - 엔티티가 변하면 API 스펙이 변한다.
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * - 양방향 연관관계 문제
  *
- * xToOne(ManyToOne, OneToOne) 관계 최적화
- * Order
- * Order -> Member
- * Order -> Delivery
+ * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+ * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+ *
+ * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
+ * - 페이징 가능
+ * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
+ * - 페이징 가능
+ * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
+ * - 페이징 불가능...
  *
  */
 @RestController
@@ -58,6 +70,16 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v3/simple-orders")
     public List<SimpleOrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
     }
 
     @Data
