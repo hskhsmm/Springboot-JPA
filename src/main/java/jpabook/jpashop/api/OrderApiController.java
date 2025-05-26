b75db884e4f807d7ed78598832537434c9e85b7b
hskhsmm <hskhsmm@naver.com>
2025-05-26 23:02:23 +0900

    feat: V5: 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -98,8 +98,12 @@ public class OrderApiController {
 
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
-        orderQueryRepository.findOrderQueryDtos();
+        return orderQueryRepository.findOrderQueryDtos();
+    }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
     }
 
 
